2132077fe82d953bc616296b66127419dd673793
ganeshgugulothu543 <ganeshgugulothu543@gmail.com>
2025-04-15 17:25:46 +0530

    added TodoApp in Github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+import "./components/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
